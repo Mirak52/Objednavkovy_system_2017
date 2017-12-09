fb5f959faaadb23e7b5ae4cde2afcc53e146c416
Marek Bastl <bastlma14@sps-prosek.cz>
2017-12-09 13:24:40 +0100

    login

--- Objednavkovy_system/Objednavkovy_system/MainWindow.xaml.cs
+++ Objednavkovy_system/Objednavkovy_system/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Objednavkovy_system.pages;
+using RestSharp;
 
 namespace Objednavkovy_system
 {
@@ -31,6 +32,58 @@ namespace Objednavkovy_system
             Registration page = new Registration();
             page.Show();
             this.Close();
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Email.Text == "" && Password.Password == "")
+            {
+                Error.Content = "Špatně zadaná data!!!";
+                Email.Focus();
+            }
+            else
+            {
+                bool emailPassed = true;
+                try
+                {
+                    var eMailValidator = new System.Net.Mail.MailAddress(Email.Text);
+                }
+                catch (FormatException ex)
+                {
+                    Error.Content = "Špatně zadaný email!!!";
+                    emailPassed = false;
+                }
+                if (emailPassed)
+                {
+                    Login();
+                }
+            }
+        }
+
+        private void Login()
+        {
+            var client = new RestClient("https://student.sps-prosek.cz/~bastlma14/obj/account.php");
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("action", 0);
+            request.AddParameter("email", Email.Text);
+            request.AddParameter("password", Password.Password);
+            var res = client.Execute(request);
+            if (res.ResponseStatus == ResponseStatus.Error)
+            {
+                throw new System.ArgumentException("Chyba na serveru, zkontroluj URL");
+                //Error.Content= "Chyba na serveru, zkontroluj URL");
+            }
+            if (res.Content == "0")
+            {
+                Error.Content = "Špatné heslo nebo email";
+            }
+            else
+            {
+                ShopMain page = new ShopMain();
+                page.Show();
+                this.Close();
+            }
+            
         }
     }
 }
--- Objednavkovy_system/Objednavkovy_system/pages/Registration.xaml.cs
+++ Objednavkovy_system/Objednavkovy_system/pages/Registration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,49 @@ namespace Objednavkovy_system.pages
             MainWindow page = new MainWindow();
             page.Show();
             this.Close();
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Email.Text == "" && Password.Password == "" && PasswordAgain.Password == "" && Password.Password == PasswordAgain.Password)
+            {
+                Error.Content = "Špatně zadaná data!!!";
+                Email.Focus();
+            }
+            else
+            {
+                bool emailPassed = true;
+                try
+                {
+                    var eMailValidator = new System.Net.Mail.MailAddress(Email.Text);
+                }
+                catch (FormatException ex)
+                {
+                    Error.Content = "Špatně zadaný email!!!";
+                    emailPassed = false;
+                }
+                if (emailPassed)
+                {
+                    Registrate();
+                }
+            }
+        }
+        private void Registrate()
+        {
+            var client = new RestClient("https://student.sps-prosek.cz/~bastlma14/obj/account.php");
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("action", 2);
+            request.AddParameter("email", Email.Text);
+            request.AddParameter("password", Password.Password);
+            request.AddParameter("name", Name.Text);
+            request.AddParameter("sirName", sirName.Text);
+            var res = client.Execute(request);
+            if (res.ResponseStatus == ResponseStatus.Error)
+            {
+                throw new System.ArgumentException("Chyba na serveru, zkontroluj URL");
+                //Error.Content= "Chyba na serveru, zkontroluj URL");
+            }
+            Error.Content = "Úspěšně registrován";
         }
     }
 }
